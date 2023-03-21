f2951aa218865ac5dc58df28931d8dd92d86ed3f
Dhia' Alhaq Shalabi <dhia.shalabi@gmail.com>
2023-03-21 19:10:33 +0300

    feat: completed assignment

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  const [isOn, setIsOn] = useState(false);
+  const color = isOn ? "red" : "cyan";
+
+  function handleClick() {
+    setIsOn((isOn) => !isOn);
+  }
+
+  return (
+    <button style={{
+      background: color,
+      padding: "10px 20px",
+      border: "none",
+      borderRadius: "5px",
+      fontSize: "1.5rem",
+      fontWeight: "bold",
+      cursor: "pointer",
+    }}
+      onClick={handleClick}>
+      {isOn ? "ON" : "OFF"}
+    </button>
+  );
 }
 
 export default Toggle;
